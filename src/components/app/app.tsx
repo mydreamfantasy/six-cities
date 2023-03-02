e211a4aa67d092497cf390c50068442df661688d
Anastasiia <nastyara13@yandex.ru>
2023-03-02 23:01:47 +0300

    Добавлено отображение карточек

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,10 @@
-import Home from "../../pages/home/home";
-import React from "react";
+import Home from '../../pages/home/home';
+import React from 'react';
 
 type AppScreenProps = {
   offersCount: number;
 };
 
-const App: React.FC<AppScreenProps> = ({ offersCount }) => {
-  return <Home offersCount={offersCount} />;
-};
+const App: React.FC<AppScreenProps> = ({ offersCount }) => <Home offersCount={offersCount} />;
 
 export default App;
