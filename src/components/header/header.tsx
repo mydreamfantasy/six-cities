fd590a65b6a48c1fc926b5bf788b394bedf49765
Anastasiia <nastyara13@yandex.ru>
2023-03-21 20:01:25 +0300

    Вынесена навигация

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,90 +1,22 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
 import Logo from '../logo/logo';
-import { AppRoute, AuthorizationStatus } from '../../const/const';
-import { useAppDispatch, useAppSelector } from '../../hooks';
-import { logoutAction } from '../../store/api-actions';
+import HeaderNav from '../header-nav/header-nav';
 
 type HeaderProps = {
   hasNav?: boolean;
 };
 
-const Header: React.FC<HeaderProps> = ({ hasNav = true }) => {
-  const navigate = useNavigate();
-  const dispatch = useAppDispatch();
-  const info = useAppSelector((state) => state.informationUser);
-
-  const authorizationStatus = useAppSelector(
-    (state) => state.authorizationStatus
-  );
-
-  return (
-    <header className="header">
-      <div className="container">
-        <div className="header__wrapper">
-          <div className="header__left">
-            <Logo type="header" />
-          </div>
-          {hasNav && (
-            <nav className="header__nav">
-              {authorizationStatus === AuthorizationStatus.Auth ? (
-                <ul className="header__nav-list">
-                  <li className="header__nav-item user">
-                    <Link
-                      to={AppRoute.Favorites}
-                      className="header__nav-link header__nav-link--profile"
-                    >
-                      <div className="header__avatar-wrapper user__avatar-wrapper">
-                        <img
-                          className="user__avatar"
-                          src={info ? info.avatarUrl : ''}
-                          width="54"
-                          height="54"
-                          alt="User avatar"
-                        />
-                      </div>
-                      <span className="header__user-name user__name">
-                        {info ? info.email : ''}
-                      </span>
-                      <span className="header__favorite-count">3</span>
-                    </Link>
-                  </li>
-                  <li className="header__nav-item">
-                    <a
-                      className="header__nav-link"
-                      href="/#"
-                      onClick={(event) => {
-                        event.preventDefault();
-                        dispatch(logoutAction());
-                      }}
-                    >
-                      <span className="header__signout">Sign out</span>
-                    </a>
-                  </li>
-                </ul>
-              ) : (
-                <ul className="header__nav-list">
-                  <li className="header__nav-item user">
-                    <a
-                      className="header__nav-link header__nav-link--profile"
-                      href="/#"
-                      onClick={(event) => {
-                        event.preventDefault();
-                        navigate(AppRoute.Login);
-                      }}
-                    >
-                      <div className="header__avatar-wrapper user__avatar-wrapper" />
-                      <span className="header__login">Sign in</span>
-                    </a>
-                  </li>
-                </ul>
-              )}
-            </nav>
-          )}
+const Header: React.FC<HeaderProps> = ({ hasNav = true }) => (
+  <header className="header">
+    <div className="container">
+      <div className="header__wrapper">
+        <div className="header__left">
+          <Logo type="header" />
         </div>
+        {hasNav && <HeaderNav />}
       </div>
-    </header>
-  );
-};
+    </div>
+  </header>
+);
 
 export default Header;
