9536d6b21cd06e78e6359647934de8163dbf7334
Anastasiia <nastyara13@yandex.ru>
2023-03-21 21:37:56 +0300

    Поправлен компонент формы входа

--- src/components/login-form/login-form.tsx
+++ src/components/login-form/login-form.tsx
@@ -60,59 +60,47 @@ const LoginForm: React.FC = () => {
   };
 
   return (
-    <div className="page__login-container container">
-      <section className="login">
-        <h1 className="login__title">Sign in</h1>
-        <form
-          className="login__form form"
-          action="#"
-          method="post"
-          onSubmit={handleSubmit}
-        >
-          <div className="login__input-wrapper form__input-wrapper">
-            <label className="visually-hidden">E-mail</label>
+    <form
+      className="login__form form"
+      action="#"
+      method="post"
+      onSubmit={handleSubmit}
+    >
+      <div className="login__input-wrapper form__input-wrapper">
+        <label className="visually-hidden">E-mail</label>
 
-            <input
-              className="login__input form__input"
-              type="email"
-              name="email"
-              placeholder="Email"
-              value={data.email.value}
-              onChange={handleLoginChange}
-              required
-            />
-            {data.email.error && (
-              <span className={styles.error}>{data.email.errorText}</span>
-            )}
-          </div>
-          <div className="login__input-wrapper form__input-wrapper">
-            <label className="visually-hidden">Password</label>
-            <input
-              className="login__input form__input"
-              type="password"
-              name="password"
-              placeholder="Password"
-              required
-              value={data.password.value}
-              onChange={handleLoginChange}
-            />
-            {data.password.error && (
-              <span className={styles.error}>{data.password.errorText}</span>
-            )}
-          </div>
-          <button className="login__submit form__submit button" type="submit">
-            Sign in
-          </button>
-        </form>
-      </section>
-      <section className="locations locations--login locations--current">
-        <div className="locations__item">
-          <a className="locations__item-link" href="#">
-            <span>Amsterdam</span>
-          </a>
-        </div>
-      </section>
-    </div>
+        <input
+          className="login__input form__input"
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={data.email.value}
+          onChange={handleLoginChange}
+          required
+        />
+        {data.email.error && (
+          <span className={styles.error}>{data.email.errorText}</span>
+        )}
+      </div>
+      <div className="login__input-wrapper form__input-wrapper">
+        <label className="visually-hidden">Password</label>
+        <input
+          className="login__input form__input"
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+          value={data.password.value}
+          onChange={handleLoginChange}
+        />
+        {data.password.error && (
+          <span className={styles.error}>{data.password.errorText}</span>
+        )}
+      </div>
+      <button className="login__submit form__submit button" type="submit">
+        Sign in
+      </button>
+    </form>
   );
 };
 
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -6,7 +6,19 @@ import LoginForm from '../../components/login-form/login-form';
 const Login: React.FC = () => (
   <Layout className="page--gray page--login" hasNav={false} pageTitle="Login">
     <main className="page__main page__main--login">
-      <LoginForm />
+      <div className="page__login-container container">
+        <section className="login">
+          <h1 className="login__title">Sign in</h1>
+          <LoginForm />
+        </section>
+        <section className="locations locations--login locations--current">
+          <div className="locations__item">
+            <a className="locations__item-link" href="#">
+              <span>Amsterdam</span>
+            </a>
+          </div>
+        </section>
+      </div>
     </main>
   </Layout>
 );
